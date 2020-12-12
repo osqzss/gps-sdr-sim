4ce4d0a7539f7f01e1ef1aa542ea4537dfbce646
Chuang Zhu <genelocated@yandex.com>
2020-12-13 01:16:17 +0800

    change back to web.py since it is much more faster
    And finally I found that web.py supports Python 3...

--- mapserver/mapper.py
+++ mapserver/mapper.py
@@ -1,62 +1,48 @@
-#!/usr/bin/env python3
-
-import tornado.ioloop
-import tornado.web
-from tornado.web import RequestHandler, StaticFileHandler
+#!/usr/bin/env python
+from __future__ import print_function
 import socket
 import struct
 import time
-import os
-
+import web
 HOST = 'localhost'
 PORT = 5678
+BUFFSIZE = 1024
+ADDR = (HOST, PORT)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-
-class IndexHandler(RequestHandler):
-    def get(self):
-        self.redirect('/static/baidumap.html')
-
-
-class PostHandler(RequestHandler):
-    def post(self):
-        self.set_header('Content-Type', 'text/plain')
-        pos = [
-            float(self.get_body_argument(k)) for k in ['lat', 'lon', 'hgt']
-        ]
-        data = struct.pack('ddd', *pos)
-        sock.sendto(data, (HOST, PORT))
-        print(*pos)
-        self.write('OK')
+urls = (
+    '/', 'index',
+    '/post', 'post',
+    '/static/*', 'static',
+)
+app = web.application(urls, globals())
 
 
-def make_app():
-    return tornado.web.Application([
-        (r'/', IndexHandler),
-        (r'/post', PostHandler),
-        (r'/static/(.*)', StaticFileHandler, {
-            'path': os.path.dirname(__file__)
-        })
-    ])
+class index:
+    def GET(self):
+        raise web.seeother('/static/baidumap.html')
 
 
-if __name__ == "__main__":
-    from argparse import ArgumentParser
-    argp = ArgumentParser(description='GPS-SDR-SIM realtime map server.')
-    argp.add_argument('port',
-                      type=int,
-                      default=8080,
-                      nargs='?',
-                      help='specify port for map server [default: 8080]')
-    argp.add_argument(
-        '--host',
-        type=str,
-        default='0.0.0.0',
-        help='specify host map server to bind [default: 0.0.0.0]')
-    args = argp.parse_args()
+class post:
+    def POST(self):
+        inn = web.input()
+        lon = float(inn.get('lon'))
+        lat = float(inn.get('lat'))
+        h = float(inn.get('hgt'))
+        data = struct.pack('ddd', lat, lon, h)
+        sock.sendto(data, ADDR)
+        print(lon, lat, h)
 
-    app = make_app()
-    print('Serving at http://{}:{}'.format(args.host, args.port))
-    app.listen(args.port, args.host)
-    tornado.ioloop.IOLoop.current().start()
+
+class static:
+    def GET(self, media, file):
+        try:
+            f = open(media + '/' + file, 'r')
+            return f.read()
+        except:
+            return '404'
+
+
+if __name__ == '__main__':
+    app.run()
