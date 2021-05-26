399579f352c15dc8e30cf12eeee801f6909221d4
bt-juanfandino1234 <juan.ospina@beamtrail.com>
2021-05-26 16:17:08 +0400

    Revert "Time of ephemeris changes to fit our spoofer requirements. The ephemeris shall be valid for 4 hours, 2 hours ahead and 2 hours in the past."
    This reverts commit b80826d7e647b5b5c6cb61f4239729cc3e5e0f0e.

--- gpssim.c
+++ gpssim.c
@@ -2009,7 +2009,7 @@ int main(int argc, char *argv[])
 		}
 		else
 		{
-			if (fabs(subGpsTime(g0, gmin))>2*SECONDS_IN_HOUR && fabs(subGpsTime(gmax, g0))>2*SECONDS_IN_HOUR)
+			if (subGpsTime(g0, gmin)<0.0 || subGpsTime(gmax, g0)<0.0)
 			{
 				fprintf(stderr, "ERROR: Invalid start time.\n");
 				fprintf(stderr, "tmin = %4d/%02d/%02d,%02d:%02d:%02.0f (%d:%.0f)\n", 
@@ -2042,7 +2042,7 @@ int main(int argc, char *argv[])
 			if (eph[i][sv].vflg == 1)
 			{
 				dt = subGpsTime(g0, eph[i][sv].toc);
-				if (dt>=-2*SECONDS_IN_HOUR && dt<2*SECONDS_IN_HOUR)
+				if (dt>=-SECONDS_IN_HOUR && dt<SECONDS_IN_HOUR)
 				{
 					ieph = i;
 					break;
